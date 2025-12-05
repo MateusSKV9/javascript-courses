7da9b736be242f1f4bc8be19f4338e8c82553324
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 11:11:06 -0300

    feat(course-matheus-battisti): introduction to string

---
+++ course-matheus-battisti/ex005-strings/script.js
@@ -0,0 +1,21 @@
+var nome = "Mateus";
+var sobrenome = "Santos";
+
+console.log(nome + " " + sobrenome);
+
+console.log(typeof nome);
+
+console.log(typeof "abc");
+console.log(typeof "12");
+
+var frase = "Frase";
+console.log(frase);
+console.log(typeof frase);
+
+const p = document.createElement("p");
+p.textContent = "Olá, Mundo!";
+document.body.appendChild(p);
+
+document.write("Conteúdo escrito no documento por meio de uma script externo");
+
+document.write("<br>Nova escrita: " + frase);
