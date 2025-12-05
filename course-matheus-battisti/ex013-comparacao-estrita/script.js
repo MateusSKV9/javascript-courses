cf112f6bba753804cc1e6d9ccc42f6bd9901d912
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 20:05:40 -0300

    feat(course-matheus-battisti): comparacao estrita

---
+++ course-matheus-battisti/ex013-comparacao-estrita/script.js
@@ -0,0 +1,10 @@
+var numero = 15;
+var numeroString = "15";
+
+if (numero == numeroString) {
+	console.log("Mesmo conteúdo e tipos iguais");
+} else if (numero == numeroString) {
+	console.log("Mesmo contéudo e tipos diferentes");
+} else {
+	console.log("Conteúdo diferentes");
+}
