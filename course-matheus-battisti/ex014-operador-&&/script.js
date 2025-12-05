278e36a2d686a1b16d6fd9a8446096647e0c8949
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 20:11:54 -0300

    feat(course-matheus-battisti): operador &&

---
+++ course-matheus-battisti/ex014-operador-&&/script.js
@@ -0,0 +1,8 @@
+var idade = 16;
+var nome = "Mateus";
+
+if (nome === "Mateus" && idade) console.log("O " + nome + " pode entrar na aula de esgrima");
+else console.log("Este não é o " + nome);
+
+if (1 == 1 && 3 > 2 && true) console.log("Passou");
+else if (nome === "Mateus" && idade >= 14) console.log("Aqui passa");
