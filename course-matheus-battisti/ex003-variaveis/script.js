d1efdb92a243c54179f1417faf2b5c8ae2f800ba
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 11:09:29 -0300

    feat(course-matheus-battisti): variables

---
+++ course-matheus-battisti/ex003-variaveis/script.js
@@ -0,0 +1,14 @@
+var nome = "Mateus";
+console.log(nome);
+
+var $nome = "Ana";
+console.log($nome);
+
+var _nome = "Maria";
+console.log(_nome);
+
+var primeiroNome = "João";
+console.log(primeiroNome);
+
+var nome5 = "Vite";
+console.log(nome5);
