5d8d9720b10203ac5adb70ce5a85afdabc63a601
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 19:29:25 -0300

    feat(course-matheus-battisti): comparacao

---
+++ course-matheus-battisti/ex011-comparacao/script.js
@@ -0,0 +1,12 @@
+var number1 = 5;
+var number2 = 7;
+
+if (number1 > number2) console.log(number1 + " > " + number2);
+else if (number2 > number1) console.log(number2 + " > " + number1);
+else console.log(number1 + " == " + number2);
+
+var number3 = 3;
+var number4 = 2;
+
+if (number3 != number4) console.log(number3 + " != " + number4);
+else console.log(number3 + " == " + number4);
