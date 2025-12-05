6df1ca1877be518ab751227f831edddf9408cab9
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-05 11:10:23 -0300

    feat(course-matheus-battisti): data types and typeof

---
+++ course-matheus-battisti/ex004-tipos-de-dados/script.js
@@ -0,0 +1,20 @@
+var numero = 5;
+console.log(numero);
+console.log(typeof numero);
+
+var numeroFloat = 5.25;
+console.log(numeroFloat);
+console.log(typeof numeroFloat);
+
+var numeroString = "123";
+console.log(numeroString);
+console.log(typeof numeroString);
+
+console.log(NaN);
+console.log(typeof NaN);
+console.log(undefined);
+console.log(typeof undefined);
+console.log(null);
+console.log(typeof null);
+console.log(Infinity);
+console.log(typeof Infinity);
