37e1570902e4e363bc8157ddc0f9c41a03e39fa9
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 12:37:17 -0300

    feat(course-matheus-battisti): for

---
+++ course-matheus-battisti/ex019-for/script.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 10; i++) {
+	console.log("Repetição " + i);
+}
+
+var arr = [1, 2, 3, , 4];
+
+for (var j = 0; j < arr.length; j++) {
+	console.log(arr[j]);
+}
+
+for (var k = 0; k <= 100; k += 25) {
+	console.log(k);
+}
