98358c29c39423204438993275cf4945fafed213
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 16:20:47 -0300

    feat(course-matheus-battisti): textContent e innerHTML

---
+++ course-matheus-battisti/ex034-textContent-innerHTML/script.js
@@ -0,0 +1,9 @@
+// innerHTML
+const title = document.querySelector("#title");
+console.log(title.innerHTML);
+title.innerHTML = "Título alterado";
+
+// textContent
+const subtitle = document.querySelector(".subtitle");
+console.log(subtitle.textContent);
+subtitle.textContent = "Subtítulo alterado";
