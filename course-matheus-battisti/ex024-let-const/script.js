21055d507b7a88fc88099763c2428d1f443a038b
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 13:28:27 -0300

    feat(course-matheus-battisti): let e const

---
+++ course-matheus-battisti/ex024-let-const/script.js
@@ -0,0 +1,28 @@
+let x = 1;
+const y = 9;
+console.log(x);
+console.log(y);
+
+if (true) {
+	let x = 2;
+	const y = 8;
+	console.log(x);
+	console.log(y);
+}
+
+function show() {
+	let x = 3;
+	const y = 7;
+	console.log(x);
+	console.log(y);
+}
+
+show();
+
+for (let i = 0; i < 10; i++) {
+	console.log(i);
+}
+
+for (let i = 10; i > 0; i--) {
+	console.log(i);
+}
