f0941b7c1a56597a67ecea082932fbc25d18325b
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 14:09:45 -0300

    feat(course-matheus-battisti): métodos number

---
+++ course-matheus-battisti/ex025-metodos-number/script.js
@@ -0,0 +1,31 @@
+// parseFloat
+console.log(parseFloat(10));
+console.log(parseFloat("23.12"));
+console.log(parseFloat("13,23"));
+console.log(Number.parseFloat("4.25"));
+console.log("\n");
+
+// parseInt
+console.log(parseInt("9"));
+console.log(parseInt("29.12"));
+console.log(parseInt(9.2));
+console.log(parseInt(2, 23));
+console.log("\n");
+
+/// toFixed
+console.log((29.123).toFixed(2));
+console.log((29.123).toFixed(1));
+console.log((29.123).toFixed(0));
+console.log("\n");
+
+// isNaN
+console.log(isNaN(12));
+console.log(isNaN("12"));
+console.log(isNaN("12e"));
+console.log(isNaN("oi"));
+console.log("\n");
+
+// MAX_VALUE e MIN_VALUE
+console.log(Number.MAX_VALUE);
+console.log(2 * Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
