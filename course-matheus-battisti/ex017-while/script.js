238ea7bcf8c726bb8ded354e5a55a11d67e930d0
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 12:36:55 -0300

    feat(course-matheus-battisti): while

---
+++ course-matheus-battisti/ex017-while/script.js
@@ -0,0 +1,22 @@
+var x = 0;
+
+while (x <= 5) {
+	console.log("Repetição " + x);
+	x++;
+}
+
+var arr = ["a", "b", "c", "d"];
+var y = 0;
+
+while (y <= 3) {
+	console.log(arr[y]);
+	y++;
+}
+
+var palavra = "Mateus";
+var i = 0;
+
+while (i < palavra.length) {
+	console.log(palavra[i]);
+	i++;
+}
