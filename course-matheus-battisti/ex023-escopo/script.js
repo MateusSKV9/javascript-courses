c9e1abd615df3a3da366c75f158ba4ac4aea9eb3
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 12:38:30 -0300

    feat(course-matheus-battisti): escopo var

---
+++ course-matheus-battisti/ex023-escopo/script.js
@@ -0,0 +1,20 @@
+var x = 1;
+
+console.log(x);
+
+function show() {
+	var x = 2;
+	console.log(x);
+}
+show();
+
+console.log(x);
+
+if (true) {
+	console.log(x);
+}
+
+if (true) {
+	var x = 9;
+	console.log(x);
+}
