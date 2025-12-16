482e404246a6037bbf1078966a45bf2ba7eed7b3
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 16:42:10 -0300

    feat(course-matheus-battisti): removeChild e remove

---
+++ course-matheus-battisti/ex036-removeChild-remove/script.js
@@ -0,0 +1,11 @@
+// removeChild
+const lista = document.querySelector("#lista");
+
+const li = document.querySelector("#lista li:nth-child(4)");
+
+lista.removeChild(li);
+
+// remove
+const container = document.querySelector("#container");
+
+container.remove();
