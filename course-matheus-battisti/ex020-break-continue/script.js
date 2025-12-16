45bac50419f5f74562fab7be29d305b7fb5568ee
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 12:37:45 -0300

    feat(course-matheus-battisti): break-continue

---
+++ course-matheus-battisti/ex020-break-continue/script.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 10; i++) {
+	if (i >= 7) break;
+	console.log(i);
+}
+console.log("Finalizou");
+
+for (var j = 10; j > 0; j--) {
+	if (j == 5 || j == 3) continue;
+	console.log(j);
+}
+console.log("Finalizou");
