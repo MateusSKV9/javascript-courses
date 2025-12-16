569dacec8484920b25f82cb94ba50091cfd0a438
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-16 15:56:06 -0300

    feat(course-matheus-battisti): formas de acessar elementos dom

---
+++ course-matheus-battisti/ex032-acessando-elementos-dom/script.js
@@ -0,0 +1,18 @@
+// tag
+const title = document.getElementsByTagName("h1")[0];
+console.log(title);
+
+const uls = document.getElementsByTagName("ul");
+console.log(uls);
+
+// id
+const lista = document.getElementById("lista1");
+console.log(lista);
+
+// class
+const items = document.getElementsByClassName("item");
+console.log(items);
+
+// querySelector
+const itemLista1 = document.querySelectorAll("#lista1 > li");
+console.log(itemLista1);
