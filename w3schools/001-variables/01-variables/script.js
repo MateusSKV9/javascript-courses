cb5106861c21c7a8da9796d9bd14655f334bac58
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-19 18:00:44 -0300

    feat(w3schools): variables

---
+++ w3schools/001-variables/01-variables/script.js
@@ -0,0 +1,67 @@
+// let
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(`${x} + ${y} = ${z}`);
+
+// const
+const a = 1;
+const b = 2;
+const c = a + b;
+console.log(`${a} + ${b} = ${c}`);
+
+// _
+let _lastName = "Mateus";
+let _x = 10;
+const _100 = 100;
+const _private = "Private";
+
+// $
+let $ = "Hello, World!";
+let $a = 2;
+let _$ = 4;
+
+//
+let nome; // undefined
+const price1 = 2;
+const price2 = 3;
+let total = price1 + price2;
+console.log(total);
+
+// auto
+x1 = 1;
+x2 = 2;
+x3 = x1 + x2;
+console.log(x3);
+
+// var
+var v1 = 4;
+var v2 = 3;
+var v3 = v1 + v2;
+console.log(v3);
+
+// number
+const PI = 3.14;
+let number = 23;
+
+// string
+const name = "Mateus";
+let nome2 = "Maria";
+
+// ,
+let n1 = 1,
+	n2 = 2,
+	n3 = 3;
+
+const n4 = 4,
+	n5 = 5;
+
+// =
+n1 = 1 + 2 + 3;
+nome2 = "Mateus" + " " + "Santos";
+console.log(n1, nome2);
+
+//
+let p = "3" + 2 + 1;
+let p2 = 2 + 1 + 2 + "3";
+console.log(p, p2);
