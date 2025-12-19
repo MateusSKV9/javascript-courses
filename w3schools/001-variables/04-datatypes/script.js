91006364a19905c571ca117b8f6b894cb5c16c66
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-19 18:01:14 -0300

    feat(w3schools): datatypes

---
+++ w3schools/001-variables/04-datatypes/script.js
@@ -0,0 +1,67 @@
+// String
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Number
+let length = 16;
+let weight = 7.5;
+
+// BigInt
+let x = 1234567890123456789012345n;
+let y = BigInt(1234567890123456789012345);
+
+// Boolean
+let x1 = true;
+let y1 = false;
+
+// Object
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object
+const date = new Date("2022-03-25");
+
+// Undefined
+let x3;
+let y3;
+
+// Null
+let x4 = null;
+let y4 = null;
+
+// Symbol
+const x5 = Symbol();
+const y5 = Symbol();
+console.log(x5);
+
+// typeof
+typeof ""; // Returns "string"
+typeof "John"; // Returns "string"
+typeof "John Doe"; // Returns "string"
+typeof 0; // Returns "number"
+typeof 314; // Returns "number"
+typeof 3.14; // Returns "number"
+typeof 3; // Returns "number"
+typeof (3 + 4); // Returns "number"
+
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+// With decimals:
+let x6 = 34.0;
+
+// Without decimals:
+let x7 = 34;
+
+let x8 = 5;
+
+(x8 == 8); // equals false
+(x8 != 8); // equals true
