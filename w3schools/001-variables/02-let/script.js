5611ea8925357acde062c9b11a24e9b5f73ac4c8
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-19 18:00:53 -0300

    feat(w3schools): let

---
+++ w3schools/001-variables/02-let/script.js
@@ -0,0 +1,36 @@
+{
+	let x = 2;
+	console.log(x);
+}
+
+{
+	var x = 2;
+	console.log(x);
+}
+
+let x = 10;
+console.log(x);
+
+{
+	let x = 2;
+	console.log(x);
+}
+
+var a = 2;
+console.log(a);
+
+var x = 3;
+console.log(x);
+
+var x = 2; // Allowed
+let x = 3; // Not allowed
+
+{
+	//let x = 2; // Allowed
+	let x = 3; // Not allowed
+}
+
+{
+	let x = 2; // Allowed
+	var x = 3; // Not allowed
+}
