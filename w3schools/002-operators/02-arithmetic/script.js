3f5206565d4346a353dd91514aa2ca24c723027e
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-19 18:01:35 -0300

    feat(w3schools): arithmetic

---
+++ w3schools/002-operators/02-arithmetic/script.js
@@ -0,0 +1,68 @@
+{
+	let x = 5;
+	let y = 2;
+	let z = x + y;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let y = 2;
+	let z = x - y;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let y = 2;
+	let z = x * y;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let y = 2;
+	let z = x / y;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let y = 2;
+	let z = x % y;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	x++;
+	let z = x;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	x--;
+	let z = x;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let z = x ** 2;
+	console.log(z);
+}
+
+{
+	let x = 5;
+	let z = Math.pow(x, 2);
+	console.log(z);
+}
+
+{
+	let x1 = 100 + 50 * 3;
+	let x2 = (100 + 50) * 3;
+	let x3 = 100 + 50 - 3;
+	let x4 = (100 / 50) * 3;
+	console.log(x1, x2, x3, x4);
+}
