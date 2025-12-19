8d75ba1d610dc6e85e4bf33ecbee5b22dcd050d7
Mateus Santos <mateussilvanzg@gmail.com>
2025-12-19 18:01:23 -0300

    feat(w3schools): operator

---
+++ w3schools/002-operators/01-operators/script.js
@@ -0,0 +1,45 @@
+// Assign the value 5 to x
+let x = 5;
+// Assign the value 2 to y
+let y = 2;
+// Assign the value x + y to z:
+let z1 = x + y;
+let z2 = x * y;
+console.log(z1, z2);
+
+let a = 3;
+let x2 = (100 + 50) * a;
+console.log(x2);
+
+{
+	let text1 = "John";
+	let text2 = "Doe";
+	let text3 = text1 + " " + text2;
+	console.log(text3);
+}
+
+{
+	let text1 = "What a very ";
+	text1 += "nice day";
+	console.log(text1);
+}
+
+{
+	let x = 5 + 5;
+	let y = "5" + 5;
+	let z = "Hello" + 5;
+	console.log(x, y, z);
+}
+
+{
+	let x = 10;
+	x += 5;
+	console.log(x);
+}
+
+{
+	let text1 = "A";
+	let text2 = "B";
+	let result = text1 < text2;
+	console.log(result);
+}
